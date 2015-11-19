7d6e30c50d083e73ba556477d7b03aa159c76d04
Porawit Poboonma <ball6847@gmail.com>
2015-11-20 00:47:04 +0700

    add doctrine migration to console runner

--- cli-config.php
+++ cli-config.php
@@ -1,14 +1,20 @@
 <?php
-
+/**
+ *  Doctrine Console Runner
+ *
+ *  See available commands by running: ./vendor/bin/doctrine
+ */
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 // load Slim application, we need $doctrine DoctrineMiddleware
 $app = require(__DIR__."/app/bootstrap.php");
 
-// setup middleware
-// $doctrine->setup();
-
-// create entityManager
-// $em = $doctrine->createEntityManager();
-
-return ConsoleRunner::createHelperSet($app->doctrine);
+// create doctrine console application
+ConsoleRunner::run(ConsoleRunner::createHelperSet($app->doctrine), [
+    new Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand,
+    new Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand,
+    new Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand,
+    new Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand,
+    new Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand,
+    new Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand,
+]);
